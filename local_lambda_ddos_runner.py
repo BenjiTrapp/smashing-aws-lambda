3744a9eb42e7e87bf51205b137214f543b8d0b1d
Benjamin-Yves Trapp <benji.trapp@gmail.com>
2022-02-23 14:17:05 +0100

    Update local_lambda_ddos_runner.py

--- local_lambda_ddos_runner.py
+++ local_lambda_ddos_runner.py
@@ -6,7 +6,7 @@ lam = boto3.client('lambda')
 for x in range(1000):
     print(x)
     response = lam.invoke(
-            Payload=json.dumps({"url":<Your Website>, "num_requests": 1000}).encode(),
+        Payload=json.dumps({"url":<Your Website>, "num_requests": 1000}).encode(),
         FunctionName='ddostest',
         InvocationType='Event'
     )
